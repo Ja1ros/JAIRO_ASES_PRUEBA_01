ca5b9b7f34b24a53834df5350b8334d409a84456
jairo ases <jaiacs.20@gmail.com>
2022-12-01 18:55:37 -0500

    commit 19:59

--- app/src/main/java/com/example/jairo_ases_prueba_01/MainActivity3.java
+++ app/src/main/java/com/example/jairo_ases_prueba_01/MainActivity3.java
@@ -6,22 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity3 extends AppCompatActivity {
+
+    private EditText etDividendo;
+    private EditText etDivisor;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main3);
 
-        Button buttonNext = findViewById(R.id.buttonNext);
-        buttonNext.setOnClickListener(new View.OnClickListener() {
+        Button buttonCerrar = findViewById(R.id.buttonCerrar);
+        buttonCerrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent (v.getContext(), MainActivity2.class);
+                Intent intent = new Intent (v.getContext(), MainActivity.class);
                 startActivityForResult(intent, 0);
             }
         });
+
     }
 }
